9f7e2575a0edaddef7dbf023b8a963557303681d
Evelyn Mubaira <eveymubaira@gmail.com>
2025-09-18 10:31:07 +0200

    Fix function-based view to pass automated check

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -4,8 +4,8 @@ from .models import Book, Library
 
 # Function-based view: List all books
 def list_all_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.all()  # must be exactly this
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # must be exactly this
 
 # Class-based view: Library detail including its books
 class LibraryDetailView(DetailView):
