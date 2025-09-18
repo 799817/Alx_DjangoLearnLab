8e5aad720ab5d7774ebb9dee04b6223f38851a58
Evelyn Mubaira <eveymubaira@gmail.com>
2025-09-18 10:41:00 +0200

    Fix function-based view to render list of books and update templates/urls

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -3,9 +3,9 @@ from django.views.generic.detail import DetailView
 from .models import Book, Library
 
 # Function-based view: List all books
-def list_all_books(request):
-    books = Book.objects.all()  # must be exactly this
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # must be exactly this
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view: Library detail including its books
 class LibraryDetailView(DetailView):
